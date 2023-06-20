ebbcc8f0256f98f6469ec8841fc3a40a9f073fca
Ajay Anand <writetoajayanand@gmail.com>
2023-06-20 15:10:08 -0400

    get user input for RobotIP

--- ohmniConnect.bash
+++ ohmniConnect.bash
@@ -1,7 +1,7 @@
 #pKeyPath stores the path to the private key.
 pKeyPath="~/.ssh/rrl_ohmnilabs_1"
 #robotIP stores the local IP address of the robot.
-robotIP=10.152.96.105
-echo $robotIP
+echo Please enter the robot IP
+read robotIP
 echo "ssh into root@$robotIP using private key $pKeyPath"
 ssh -i $pKeyPath "root@$robotIP"
