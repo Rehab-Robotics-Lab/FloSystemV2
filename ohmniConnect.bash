4d0eb65ab1d825cbf826e6252e4a4b2268cee393
Ajay Anand <writetoajayanand@gmail.com>
2023-06-20 15:43:56 -0400

    bash script mod, IP input

--- ohmniConnect.bash
+++ ohmniConnect.bash
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 #pKeyPath stores the path to the private key.
 pKeyPath="~/.ssh/rrl_ohmnilabs_1"
-#robotIP stores the local IP address of the robot.
-echo Please enter the robot IP
-read robotIP
-echo "ssh into root@$robotIP using private key $pKeyPath"
+#robotIP stores the local IP address of the robot
+read -p 'robot IP: ' robotIP
+read -p 'Would you like to enter custom path to private key?(yes/no) ' cusPath
+if [ $cusPath = "yes" ]; then
+    read -p 'Private Key: ' pKeyPath
+fi    
+echo "ssh into root@$robotIP using private key at $pKeyPath"
 ssh -i $pKeyPath "root@$robotIP"
