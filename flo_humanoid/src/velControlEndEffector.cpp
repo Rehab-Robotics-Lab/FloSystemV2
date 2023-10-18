1693c5731c9d5c5c804071ee6508ba3bdd47ac72
Ajay Anand <writetoajayanand@gmail.com>
2023-10-18 15:47:09 -0400

    add end effector velocity control functions

--- flo_humanoid/src/velControlEndEffector.cpp
+++ flo_humanoid/src/velControlEndEffector.cpp
@@ -1,2 +1,116 @@
 // need to look at the control table from the robotis website to get the control table location for each motor for the velocity control RAM address 
-// for eg: if using the XM430-W350 motor, control table is at https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/
+// for eg: if using the XL430-W250 motor, control table is at https://emanual.robotis.com/docs/en/dxl/x/xl430-w250/
+
+//Velocity control mode is the most appropriate for end effector control as multiple rotations of the motor are required to pull the control cables to the desired position. 
+//Test by altering the time of rotation to dial in the time for the gripper to close at each velocity.
+
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"
+
+using namespace dynamixel;
+
+// Control table address
+#define OPERATING_MODE 11
+//Set operating mode to 1 for velocity control mode(wheel mode) and 3 for position control mode(joint mode)
+#define ADDR_TORQUE_ENABLE    64
+#define ADDR_PRESENT_LED      65
+#define ADDR_GOAL_POSITION    116
+#define ADDR_PRESENT_POSITION 132
+#define ADDR_GOAL_VELOCITY    104
+#define ADDR_GOAL
+
+// Protocol version
+#define PROTOCOL_VERSION      2.0             // Default Protocol version of DYNAMIXEL X series.
+
+// Default setting
+#define DXL1_ID               1               // DXL1 ID
+#define DXL2_ID               2               // DXL2 ID
+#define BAUDRATE              57600           // Default Baudrate of DYNAMIXEL X series
+//set up fixed mount point for the device, this is the same as the one set in the udev rules file.
+#define DEVICE_NAME           "/dev/ttyUSB0"  // [Linux] To find assigned port, use "$ ls /dev/ttyUSB*" command
+
+bool getPresentPositionCallback(
+  dynamixel_sdk_examples::GetPosition::Request & req,
+  dynamixel_sdk_examples::GetPosition::Response & res)
+{
+  uint8_t dxl_error = 0;
+  int dxl_comm_result = COMM_TX_FAIL;
+
+  // Position Value of X series is 4 byte data. For AX & MX(1.0) use 2 byte data(int16_t) for the Position Value.
+  int32_t position = 0;
+
+  // Read Present Position (length : 4 bytes) and Convert uint32 -> int32
+  // When reading 2 byte data from AX / MX(1.0), use read2ByteTxRx() instead.
+  dxl_comm_result = packetHandler->read4ByteTxRx(
+    portHandler, (uint8_t)req.id, ADDR_PRESENT_POSITION, (uint32_t *)&position, &dxl_error);
+  if (dxl_comm_result == COMM_SUCCESS) {
+    ROS_INFO("getPosition : [ID:%d] -> [POSITION:%d]", req.id, position);
+    res.position = position;
+    return true;
+  } else {
+    ROS_INFO("Failed to get position! Result: %d", dxl_comm_result);
+    return false;
+  }
+}
+
+void setPositionCallback(const dynamixel_sdk_examples::SetPosition::ConstPtr & msg)
+{
+  uint8_t dxl_error = 0;
+  int dxl_comm_result = COMM_TX_FAIL;
+
+  // Position Value of X series is 4 byte data. For AX & MX(1.0) use 2 byte data(uint16_t) for the Position Value.
+  uint32_t position = (unsigned int)msg->position; // Convert int32 -> uint32
+
+  // Write Goal Position (length : 4 bytes)
+  // When writing 2 byte data to AX / MX(1.0), use write2ByteTxRx() instead.
+  dxl_comm_result = packetHandler->write4ByteTxRx(
+    portHandler, (uint8_t)msg->id, ADDR_GOAL_POSITION, position, &dxl_error);
+  if (dxl_comm_result == COMM_SUCCESS) {
+    ROS_INFO("setPosition : [ID:%d] [POSITION:%d]", msg->id, msg->position);
+  } else {
+    ROS_ERROR("Failed to set position! Result: %d", dxl_comm_result);
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  uint8_t dxl_error = 0;
+  int dxl_comm_result = COMM_TX_FAIL;
+
+  portHandler = PortHandler::getPortHandler(DEVICE_NAME);
+  packetHandler = PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+  if (!portHandler->openPort()) {
+    ROS_ERROR("Failed to open the port!");
+    return -1;
+  }
+
+  if (!portHandler->setBaudRate(BAUDRATE)) {
+    ROS_ERROR("Failed to set the baudrate!");
+    return -1;
+  }
+
+  dxl_comm_result = packetHandler->write1ByteTxRx(
+    portHandler, DXL1_ID, ADDR_TORQUE_ENABLE, 1, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS) {
+    ROS_ERROR("Failed to enable torque for Dynamixel ID %d", DXL1_ID);
+    return -1;
+  }
+
+  dxl_comm_result = packetHandler->write1ByteTxRx(
+    portHandler, DXL2_ID, ADDR_TORQUE_ENABLE, 1, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS) {
+    ROS_ERROR("Failed to enable torque for Dynamixel ID %d", DXL2_ID);
+    return -1;
+  }
+
+  ros::init(argc, argv, "read_write_node");
+  ros::NodeHandle nh;
+  ros::ServiceServer get_position_srv = nh.advertiseService("/get_position", getPresentPositionCallback);
+  ros::Subscriber set_position_sub = nh.subscribe("/set_position", 10, setPositionCallback);
+  ros::spin();
+
+  portHandler->closePort();
+  return 0;
+}
