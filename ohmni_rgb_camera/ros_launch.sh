09f344f05dc57aa8fd65bd0560230aa5bea0d69b
Ajay Anand <writetoajayanand@gmail.com>
2023-10-24 14:23:27 -0400

    roslaunch rgb cams setup

--- ohmni_rgb_camera/ros_launch.sh
+++ ohmni_rgb_camera/ros_launch.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 #
-# Setup a work space called `work` with three windows
+# Setup a work space called `work`
 session="work"
 
 # set up tmux
 tmux start-server
 
-# create a new tmux session, starting vim from a saved session in the new window
+# create a new tmux session
 tmux new-session -d -s $session -n base
-#this will start a new roscore in the first window, use this for testing the subsystem independently
-# tmux send-keys "roscore" C-m
+#This will start a new roscore if no remote roscore has been detected
+#Ideally we should change the .bashrc file to reflect the IP of the remote master and roscore will connect to remote master
+tmux send-keys "roscore" C-m
 
-#
-
-
+# launch first camera after sleeping for 20 seconds 
 tmux selectp -t 0
-tmux splitw -h -p 60
-tmux send-keys "sleep 5; roslaunch /maincam_v4l2.launch" C-m
+tmux splitw -h -p 50
+tmux send-keys "sleep 20; roslaunch /maincam_v4l2.launch" C-m
+
+
 # launch second camera after sleeping for 25 seconds
-# tmux send-keys "sleep 25; roslaunch /maincam_v4l2.launch" C-m
-
 tmux selectp -t 0
+tmux splitw -v -p 50
+tmux send-keys "sleep 25; roslaunch /maincam_v4l2.launch" C-m
+
+tmux selectp -t 1
 tmux splitw -v -p 50
 tmux send-keys "htop" C-m
