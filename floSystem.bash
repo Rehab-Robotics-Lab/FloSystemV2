76a757a8fca391786c8c51d79c805665c74fbed8
Ajay Anand <writetoajayanand@gmail.com>
2023-10-26 10:48:12 -0400

    added else for storage connection and tested script

--- floSystem.bash
+++ floSystem.bash
@@ -1,12 +1,15 @@
+echo "checking if external storage is connected"
 if test -f dev/block/sda1; then
   echo "Nvme Storage is connected, mounting at mnt/nvmeStorage"
   mkdir -p mnt/nvmeStorage
   mount dev/block/sda1 mnt/nvmeStorage
+else
+    echo "Nvme Storage is not connected"  
 fi
 #remove all stopped containers, along with any networks not used by at least one container, all dangling images, and all build cache. 
 echo "Pruning docker system"
-docker system prune
+docker system prune -f
 echo "Starting flo_system container"
-# image=flosystem/ohmni_ros:ohmni_ros_tbcontrol_0.0.13_mod
-image=ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.13
+# image=ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.13
+image=flosystem/ohmni_ros:ohmni_ros_tbcontrol_0.0.13_mod
 docker run -it -d --name flo_system --network host --privileged -v /dev:/dev -v /var/dockerhome:/home/ohmnidev $image
