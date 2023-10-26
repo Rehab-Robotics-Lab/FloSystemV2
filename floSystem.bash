1e820df6cce753ba7f9c94e9da59dff6d30507d6
Ajay Anand <writetoajayanand@gmail.com>
2023-10-26 10:31:06 -0400

    add test for device connection

--- floSystem.bash
+++ floSystem.bash
@@ -1,4 +1,11 @@
-mkdir -p mnt/nvmeStorage
-mount dev/block/sda1 mnt/nvmeStorage
-image=flosystem/ohmni_ros:ohmni_ros_tbcontrol_0.0.13_mod
+if test -f dev/block/sda1; then
+  echo "Nvme Storage is connected, mounting at mnt/nvmeStorage"
+  mkdir -p mnt/nvmeStorage
+  mount dev/block/sda1 mnt/nvmeStorage
+fi
+echo "Pruning all stopped docker containers"
+docker container prune 
+echo "Starting flo_system container"
+# image=flosystem/ohmni_ros:ohmni_ros_tbcontrol_0.0.13_mod
+image = ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.13_mod
 docker run -it -d --name flo_system --network host --privileged -v /dev:/dev -v /var/dockerhome:/home/ohmnidev $image
