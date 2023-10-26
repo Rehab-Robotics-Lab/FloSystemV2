55acb630067806e3d97b0bf6bf399bfbcd3f8ce9
Ajay Anand <writetoajayanand@gmail.com>
2023-10-26 10:33:08 -0400

    edit image name

--- floSystem.bash
+++ floSystem.bash
@@ -7,5 +7,5 @@ echo "Pruning all stopped docker containers"
 docker container prune 
 echo "Starting flo_system container"
 # image=flosystem/ohmni_ros:ohmni_ros_tbcontrol_0.0.13_mod
-image = ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.13_mod
+image=ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.13
 docker run -it -d --name flo_system --network host --privileged -v /dev:/dev -v /var/dockerhome:/home/ohmnidev $image
