152fb67862353283cd8473ff8ec30bf999a442de
Ajay Anand <writetoajayanand@gmail.com>
2023-11-10 17:51:49 -0500

    final commit motor control demo

--- motor_control_demo/src/motorControlDemo.py
+++ motor_control_demo/src/motorControlDemo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import PySimpleGUI as psg
 import rospy
 from std_msgs.msg import String
@@ -16,30 +17,29 @@ def motorGUI():
     key='-TEXT-', font=('Arial Bold', 20),
     size=(50, 2), relief="raised", border_width=5,
     expand_x=True, justification='center')],
-
     [psg.Slider(range=(0, 1000), default_value=0,
     expand_x=True, enable_events=True,
-    orientation='horizontal', key='Motor1')]
+    orientation='horizontal', key='Motor1')],
     [psg.Slider(range=(0, 1000), default_value=0,
     expand_x=True, enable_events=True,
-    orientation='horizontal', key='Motor2')]
+    orientation='horizontal', key='Motor2')],
     [psg.Slider(range=(0, 1000), default_value=0,
     expand_x=True, enable_events=True,
-    orientation='horizontal', key='Motor3')]
+    orientation='horizontal', key='Motor3')],
     [psg.Slider(range=(0, 1000), default_value=0,
     expand_x=True, enable_events=True,
     orientation='horizontal', key='Motor4')]
         ]
        
-    window = psg.Window('Hello', layout, size=(715, 150))
+    window = psg.Window('Hello', layout, size=(715, 300))
     while not rospy.is_shutdown():
         event, values = window.read()
         print(event, values)
         if event == psg.WIN_CLOSED or event == 'Exit':
             break
         if event == 'Motor1' or event == 'Motor2' or event == 'Motor3' or event == 'Motor4':
-            pub.publish(SetJointPositions(id1, id2, id3, id4, 'position', 'position', 'position', 'position', values['Motor1'], values['Motor2'], values['Motor3'], values['Motor4']))
-        rospy.loginfo("publishing to /set_joint_positions values: %s", values)
+            pub.publish(SetJointPositions(id1, id2, id3, id4, 'position', 'position', 'position', 'position', int(values['Motor1']), int(values['Motor2']), int(values['Motor3']), int(values['Motor4'])))
+            rospy.loginfo("publishing to /set_joint_positions values: %s", values)
     window.close()
     rate.sleep()
 
