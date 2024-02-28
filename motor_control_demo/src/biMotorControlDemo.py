97a6235d56fdd918508b0dbf0411eb7ed4dc854b
Ajay Anand <writetoajayanand@gmail.com>
2024-02-27 16:13:55 -0800

    bimotor control demo

--- motor_control_demo/src/biMotorControlDemo.py
+++ motor_control_demo/src/biMotorControlDemo.py
@@ -4,16 +4,20 @@ import rospy
 from std_msgs.msg import String
 from flo_humanoid.msg import SetArmsJointPositions
 
-def motorGUI():
+def biMotorGUI():
     id1 = 10
     id2 = 11
     id3 = 12
     id4 = 13
-    pub = rospy.Publisher("/set_joint_positions", SetJointPositions, queue_size=10)
-    rospy.init_node('MotorGUI', anonymous=True)
+    id5 = 20
+    id6 = 21
+    id7 = 22
+    id8 = 23
+    pub = rospy.Publisher("/set_arms_joint_positions", SetArmsJointPositions, queue_size=10)
+    rospy.init_node('BiMotorGUI', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     layout = [
-    [psg.Text('Flo V2 Motor Control Demo', enable_events=True,
+    [psg.Text('Flo V2 Bilateral Motor Control Demo', enable_events=True,
     key='-TEXT-', font=('Arial Bold', 20),
     size=(50, 2), relief="raised", border_width=5,
     expand_x=True, justification='center')],
@@ -29,23 +33,39 @@ def motorGUI():
     [psg.Slider(range=(0, 1000), default_value=0,
     expand_x=True, enable_events=True,
     orientation='horizontal', key='Motor4')]
+    [psg.Slider(range=(0, 1000), default_value=0,
+    expand_x=True, enable_events=True,
+    orientation='horizontal', key='Motor5')]
+    [psg.Slider(range=(0, 1000), default_value=0,
+    expand_x=True, enable_events=True,
+    orientation='horizontal', key='Motor6')]
+    [psg.Slider(range=(0, 1000), default_value=0,
+    expand_x=True, enable_events=True,
+    orientation='horizontal', key='Motor7')]
+    [psg.Slider(range=(0, 1000), default_value=0,
+    expand_x=True, enable_events=True,
+    orientation='horizontal', key='Motor8')]
         ]
        
-    window = psg.Window('Hello', layout, size=(715, 300))
+    window = psg.Window('BiMotorControl', layout, size=(715, 300))
     while not rospy.is_shutdown():
         event, values = window.read()
         print(event, values)
         if event == psg.WIN_CLOSED or event == 'Exit':
             break
-        if event == 'Motor1' or event == 'Motor2' or event == 'Motor3' or event == 'Motor4':
-            pub.publish(SetJointPositions(id1, id2, id3, id4, 'position', 'position', 'position', 'position', int(values['Motor1']), int(values['Motor2']), int(values['Motor3']), int(values['Motor4'])))
-            rospy.loginfo("publishing to /set_joint_positions values: %s", values)
+        if (event == 'Motor1' or event == 'Motor2' or event == 'Motor3' or event == 'Motor4' 
+            or event == 'Motor5' or event == 'Motor6' or event == 'Motor7' or event == 'Motor8'):
+            pub.publish(SetArmsJointPositions(id1, id2, id3, id4, id5, id6, id7,id8, 
+                                              'position', 'position', 'position', 'position','position', 'position', 'position', 'position',
+                                                int(values['Motor1']), int(values['Motor2']), int(values['Motor3']), int(values['Motor4'],
+                                                    int(values['Motor5']), int(values['Motor6']), int(values['Motor7']), int(values['Motor8']                                                                      )))
+            rospy.loginfo("publishing to /set_arms_joint_positions values: %s", values)
     window.close()
     rate.sleep()
 
 if __name__ == '__main__':
 
     try:
-        motorGUI()
+        biMotorGUI()
     except rospy.ROSInterruptException:
         pass
