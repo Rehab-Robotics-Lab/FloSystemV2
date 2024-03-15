f8eb2e16e913333e6e0f1463dcf9f7f50b846946
Ajay Anand <writetoajayanand@gmail.com>
2024-03-15 16:35:29 -0400

    add comment

--- motor_control_demo/run_motor_demo.bash
+++ motor_control_demo/run_motor_demo.bash
@@ -1,3 +1,4 @@
+#source the workspace
 source devel/setup.bash 
 #setup connection the the remote roscore using its IP address, make the IP address visible to all the containers, set the IP static if possible, else make it a parameter passed on during the docker run command to all containers.\
 #export ROS_MASTER_URI=http://
