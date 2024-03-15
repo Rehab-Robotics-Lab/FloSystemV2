a3d3b15da20e2dd0addb4034c784a67e477ae2e0
Ajay Anand <writetoajayanand@gmail.com>
2024-03-15 16:10:18 -0400

    debug

--- motor_control_demo/motorControlDemo.bash
+++ motor_control_demo/motorControlDemo.bash
@@ -1,3 +1,3 @@
 docker build ./ -t ajyanand/motor_control_demo:latest
-docker run -it --privileged --device=/dev/ttyUSB0 --net=host --env="DISPLAY" ajyanand/motor_control_demo:latest
-
+docker run -it --privileged --device=/dev/ttyUSB0 ajyanand/motor_control_demo:latest
+# --net=host --env="DISPLAY" 
