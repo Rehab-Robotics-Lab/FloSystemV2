1b1dd7b34e5c474958109133d591d832e5956370
Ajay Anand <writetoajayanand@gmail.com>
2024-03-15 17:01:17 -0400

    debug docker display issue

--- motor_control_demo/motorControlDemo.bash
+++ motor_control_demo/motorControlDemo.bash
@@ -1,3 +1,3 @@
 docker build ./ -t ajyanand/motor_control_demo:latest
-docker run -it --privileged --device=/dev/ttyUSB0 --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" ajyanand/motor_control_demo:latest
+docker run -it --privileged --device=/dev/ttyUSB0 --net=host -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix:rw ajyanand/motor_control_demo:latest
 
