989c6700470bd3ac044e666ce2fb88604a7c4ab4
Ajay Anand <writetoajayanand@gmail.com>
2024-03-15 15:58:59 -0400

    create setup bash script

---
+++ motor_control_demo/setup.bash
@@ -0,0 +1,13 @@
+source devel/setup.bash # source the workspace
+#setup connection the the remote roscore using its IP address, make the IP address visible to all the containers, set the IP static if possible, else make it a parameter passed on during the docker run command to all containers.\
+#export ROS_MASTER_URI=http://
+#export ROS_IP=
+#export ROS_HOSTNAME=
+cd /home/catkin_ws
+tmux new -s motor_control_demo
+## run roscore if running as a standalone container
+roscore
+tmux split-window;                               
+rosrun flo_humanoid read_write_arm_node       # run the unilateral arm control node
+tmux split-window;
+rosrun motor_control_demo motor_control_demo_node
