89f6e7f1fb1b957b64111590986337beeaa84ca1
Ajay Anand <writetoajayanand@gmail.com>
2024-03-15 16:18:36 -0400

    edit bash script, add docker options

--- motor_control_demo/motorControlDemo.bash
+++ motor_control_demo/motorControlDemo.bash
@@ -1,3 +1,3 @@
 docker build ./ -t ajyanand/motor_control_demo:latest
-docker run -it --privileged --device=/dev/ttyUSB0 ajyanand/motor_control_demo:latest
-# --net=host --env="DISPLAY" 
+docker run -it --privileged --device=/dev/ttyUSB0 --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" ajyanand/motor_control_demo:latest
+
