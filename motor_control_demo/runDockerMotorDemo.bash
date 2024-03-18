bcab836658a78da5fda3bf3896821b67409c0387
Ajay Anand <writetoajayanand@gmail.com>
2024-03-18 16:32:08 -0400

    edit bash script to run docker container with display access

--- motor_control_demo/runDockerMotorDemo.bash
+++ motor_control_demo/runDockerMotorDemo.bash
@@ -1,3 +1,10 @@
 docker build ./ -t ajyanand/motor_control_demo:latest
-docker run -it --privileged --device=/dev/ttyUSB0 --net=host -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix:rw ajyanand/motor_control_demo:latest
-
+docker container prune -f
+xhost +
+docker run -it --privileged --name flo_v2_motor_control_demo --device=/dev/ttyUSB0 --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix ajyanand/motor_control_demo:latest
+xhost -
+### error trackback 
+# File "/usr/lib/python3.6/tkinter/__init__.py", line 2023, in __init__
+#     self.tk = _tkinter.create(screenName, baseName, className, interactive, wantobjects, useTk, sync, use)
+# _tkinter.TclError: no display name and no $DISPLAY environment variable
+###
