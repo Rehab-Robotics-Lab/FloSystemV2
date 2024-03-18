a53bfe46a344eb3e3dc5f40d00161f079e3789da
Ajay Anand <writetoajayanand@gmail.com>
2024-03-18 10:23:33 -0400

    add comments + template from Flo V1

--- motor_control_demo/runMotorDemo.bash
+++ motor_control_demo/runMotorDemo.bash
@@ -1,3 +1,26 @@
+#!/bin/bash
+# container default set to bash, maybe change that 
+"""
+source ~/.bashrc
+
+tmux new-session -d -s flo
+tmux rename-window startup
+tmux send-keys 'connect_to_robot 0 && roscore' Enter
+tmux split-window -t flo -h
+
+
+tmux split-window -t flo
+tmux send-keys 'mkdir -p ~/flo_data && connect_to_robot 0 && roslaunch --wait flo_core podium_bringup.launch' Enter # it is just more stable..
+
+tmux rotate-window -t flo
+
+tmux split-window -t flo -h
+tmux send-keys 'sleep 5 && connect_to_robot 0 && roslaunch --wait flo_telepresence realsense-sp-1.launch platform:=podium' Enter # it is just more stable..
+
+tmux split-window -t flo
+tmux send-keys 'sleep 10 && connect_to_robot 0 && roslaunch --wait flo_telepresence realsense-sp-2.launch platform:=podium' Enter # it is just more stable..
+"""
+
 #source the workspace
 source devel/setup.bash 
 #setup connection the the remote roscore using its IP address, make the IP address visible to all the containers, set the IP static if possible, else make it a parameter passed on during the docker run command to all containers.\
