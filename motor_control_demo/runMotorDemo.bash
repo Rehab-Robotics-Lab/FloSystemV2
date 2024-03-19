91c3adb0124dbe3ced074cd6f8a46acd71b0ff16
Ajay Anand <writetoajayanand@gmail.com>
2024-03-19 14:15:40 -0400

    fix tmux instructions for motor control demo

--- motor_control_demo/runMotorDemo.bash
+++ motor_control_demo/runMotorDemo.bash
@@ -22,18 +22,19 @@ tmux send-keys 'sleep 10 && connect_to_robot 0 && roslaunch --wait flo_teleprese
 """
 
 #source the workspace
-source devel/setup.bash 
 #setup connection the the remote roscore using its IP address, make the IP address visible to all the containers, set the IP static if possible, else make it a parameter passed on during the docker run command to all containers.\
 #export ROS_MASTER_URI=http://
 #export ROS_IP=
 #export ROS_HOSTNAME=
-tmux new -s motor_control_demo;
+tmux new -d -s motor_control_demo;
+tmux rename-window roscore;
 tmux attach -t motor_control_demo;
-
 # run roscore if running as a standalone container
 tmux send-keys 'roscore' ENTER;
-tmux split-window;                               
-tmux send 'rosrun flo_humanoid read_write_arm_node';       # run the unilateral arm control node
+# if running as part of system connected to remote ROS master, check connection status.
+tmux split-window;    
+tmux send-keys 'source ../../devel/setup.bash' ENTER;
+tmux send-keys 'rosrun flo_humanoid read_write_arm_node' ENTER;       # run the unilateral arm control node
 tmux split-window;
-tmux send 'rosrun motor_control_demo motor_control_demo_node';
+tmux send-keys 'python3 src/motorControlDemo.py' ENTER;               # run the motor control demo
 tmux detach;
