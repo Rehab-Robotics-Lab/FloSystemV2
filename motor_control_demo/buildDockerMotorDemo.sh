2984fa23c688908a28994d9368601ebf1b788ca4
Ajay Anand <writetoajayanand@gmail.com>
2024-03-19 14:20:19 -0400

    add flags to force rebuild + pull new base image + commenting

--- motor_control_demo/buildDockerMotorDemo.sh
+++ motor_control_demo/buildDockerMotorDemo.sh
@@ -1,1 +1,4 @@
-docker build ./ -t ajyanand/motor_control_demo:latest
+docker build ./ -t --pull --no-cache ajyanand/motor_control_demo:latest
+# --pull always attempts to pull a newer version of the base image
+# --no-cache forces a rebuild of the image
+# -t tag the image with the name ajyanand/motor_control_demo:latest
