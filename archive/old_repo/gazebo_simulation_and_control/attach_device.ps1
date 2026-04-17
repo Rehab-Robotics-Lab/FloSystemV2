8cf418db5e78318278610a0a46bcc497a56482d3
anht-nguyen <tuananh.nguyen28898@gmail.com>
2026-04-17 19:14:47 -0400

    delete old repo

--- archive/old_repo/gazebo_simulation_and_control/attach_device.ps1
+++
@@ -1,6 +0,0 @@
-usbipd attach --wsl --hardware-id 0403:6014
-usbipd attach --wsl --hardware-id 046d:08e5
-usbipd attach --wsl --hardware-id 16c0:0483
-Start-Job { usbipd attach --wsl --hardware-id 0403:6014 --auto-attach }
-Start-Job { usbipd attach --wsl --hardware-id 046d:08e5 --auto-attach }
-usbipd list
